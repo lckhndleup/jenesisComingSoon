7c85b0cb34285e2a6d13774d28f9aed52f7ca985
lckhndleup <maliyildiz0307@gmail.com>
2025-09-20 21:10:33 +0300

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -328,7 +328,7 @@ export default function ComingSoon() {
             {/* Telefon - Sağ */}
             <div className="text-center md:text-right order-3 md:order-3">
               <a
-                href="tel:+902125951556"
+                href="tel:+902125951656"
                 className="inline-flex items-center gap-2 text-xs sm:text-sm text-gray-400 hover:text-[#BC461B] transition-colors duration-300"
                 style={{ fontFamily: "'Space Colony W03 SemiBold', monospace" }}
               >
@@ -340,7 +340,7 @@ export default function ComingSoon() {
                 >
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
                 </svg>
-                +90 212 595 15 56
+                +90 212 595 16 56
               </a>
             </div>
           </div>
